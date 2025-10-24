504542a752343042614821063f28cb34ff50bef1
Bohdan Boklah <b.boklag@gmail.com>
2025-10-24 16:18:00 +0300

    init сhecker time

---
+++ scripts/checker_time.py
@@ -0,0 +1,14 @@
+import time
+from functools import wraps
+
+
+def timing(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed = end_time - start_time
+        print(f"Function '{func.__name__}' executed in {elapsed:.6f} seconds")
+        return result
+    return wrapper
